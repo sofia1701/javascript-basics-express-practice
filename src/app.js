0d5c73b43efaba2229b54381834e4e332c85d627
Sofia <sofia.dionisio94@gmail.com>
2020-04-22 15:13:33 +0100

    Strings tests passing

--- src/app.js
+++ src/app.js
@@ -7,4 +7,24 @@ app.get('/strings/hello/world', (req, res) => {
   res.json({ result: 'Hello, world!' });
 });
 
+app.get('/strings/upper/hello', (req, res) => {
+  res.status(200);
+  res.json({ result: 'HELLO' });
+});
+
+app.get('/strings/lower/HELLO', (req, res) => {
+  res.status(200);
+  res.json({ result: 'hello' });
+});
+
+app.get('/strings/first-characters/hello', (req, res) => {
+  res.status(200);
+  res.json({ result: 'h' });
+});
+
+app.get('/strings/first-characters/sd32fg45', (req, res) => {
+  res.status(200);
+  res.json({ result: 'sd32' });
+});
+
 module.exports = app;
